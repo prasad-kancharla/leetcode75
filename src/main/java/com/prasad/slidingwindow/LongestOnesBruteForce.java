29e9506677c1cb47c6404ffdebc5ee5caf938b63
Prasad Kancharla <prasad.k@turing.com>
2024-12-15 12:16:45 +0530

    Solve max consecutive ones III in brute force

---
+++ src/main/java/com/prasad/slidingwindow/LongestOnesBruteForce.java
@@ -0,0 +1,36 @@
+package com.prasad.slidingwindow;
+
+// https://leetcode.com/problems/max-consecutive-ones-iii/description/
+class LongestOnesBruteForce {
+    public static void main(String[] args) {
+        int[] input = {0};
+        System.out.println(new LongestOnesBruteForce().longestOnes(input, 1));
+    }
+
+    // rewrite the problem - find a max length of sub array with at most k zeroes
+    public int longestOnes(int[] nums, int k) {
+        int maxLen = 0;
+        int n = nums.length;
+        if (k >= n) return n;
+
+        // find all sub arrays
+        for (int i = 0; i < n; i++) {
+            int zeroesInCurrentSubArray = nums[i] == 0 ? 1 : 0;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] == 0) {
+                    if (zeroesInCurrentSubArray == k) {
+                        // you cannot pick the current element as it is zero so length would be till previous element (j - 1 - i + 1)
+                        maxLen = Math.max(j - i, maxLen);
+                        break;
+                    } else {
+                        zeroesInCurrentSubArray++;
+                    }
+                }
+                // update max len when current element is 1, only when zeroes count <= k
+                if (zeroesInCurrentSubArray <= k) maxLen = Math.max(j - i + 1, maxLen);
+            }
+        }
+
+        return maxLen;
+    }
+}
